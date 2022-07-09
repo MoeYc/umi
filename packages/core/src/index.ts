f15781ff6d972f9dcd04999c4c17490a66e4fcd6
fz6m <59400654+fz6m@users.noreply.github.com>
2022-07-10 04:46:27 +0800

    test: size

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,3 +4,5 @@ export { Generator, GeneratorType } from './service/generator';
 export * from './service/pluginAPI';
 export * from './service/service';
 export { Env, IAdd, IEvent, IModify, IRoute } from './types';
+
+console.log('1233333');
